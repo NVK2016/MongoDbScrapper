330f733abf624a8c1d8622f950c82648f15efbfc
Namita Shenai <namita.shenai@gmail.com>
2019-04-18 09:29:24 -0700

    Blank schema declaration

--- models/Comment.js
+++ models/Comment.js
@@ -6,3 +6,13 @@ var mongoose = require("mongoose");
 
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
+
+//Creatting a new CommentSchema to hold all the comments related to an article  in the Mongo DB 
+var CommentSchema = new Schema({
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Comment = mongoose.model("Comment", CommentSchema);
+
+// Export the Comment model
+module.exports = Comment;
