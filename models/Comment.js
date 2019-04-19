971d286722a732ab3ffaa606b953785c2eae6a86
Namita Shenai <namita.shenai@gmail.com>
2019-04-18 22:23:35 -0700

    Added content to index.html

--- models/Comment.js
+++ models/Comment.js
@@ -9,7 +9,14 @@ var Schema = mongoose.Schema;
 
 //Creatting a new CommentSchema to hold all the comments related to an article  in the Mongo DB 
 var CommentSchema = new Schema({
+    //String type 
+    title: String, 
+
+    description: String 
 });
+
+// Remember, Mongoose will automatically save the ObjectIds of the comments
+// These ids are referred to in the Article model
 
 // This creates our model from the above schema, using mongoose's model method
 var Comment = mongoose.model("Comment", CommentSchema);
