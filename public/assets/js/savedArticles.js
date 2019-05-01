b50b714f93cb32092a83833caad9982d3e6e2ac7
Namita Shenai <namita.shenai@gmail.com>
2019-05-01 16:15:39 -0700

    Added coded for populating comments

--- public/assets/js/savedArticles.js
+++ public/assets/js/savedArticles.js
@@ -6,7 +6,7 @@ $.getJSON("/saved", function(data) {
       console.log(data[i]._id, data[i].title)
       //Create a card 
       var newArticleDiv = $("<div class='card' >"); 
-      newArticleDiv.append("<p data-id='" + data[i]._id + "'>" + "<h2 class='card-header bg-primary'>"+data[i].title+"</h2>" 
+      newArticleDiv.append("<p>" + "<h2 class='card-header bg-primary'>"+data[i].title+"</h2>" 
       + "<br />  Link Below to the TV show: <a target='_blank' href='" + data[i].link + "'> "+data[i].link +" </a><br />" +
        "<p> <img class='articleImage' src=" + data[i].image+ " /> <br/>"+
        "<a data-id='" + data[i]._id + "' class='btn btn-warning' id='deletearticle'>Delete Article</a>"
